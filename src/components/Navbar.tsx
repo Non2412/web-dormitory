37fae5e756d587710a91b5063cc170ef1e1ecb3f
Non2412 <ctf2547@gmail.com>
2025-11-25 18:57:38 +0700

    new navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,9 +43,9 @@ export default function Navbar() {
 
           <div className={styles.navLinks}>
             <Link href="/dormitory" className={styles.navLink}>Home</Link>
-            <Link href={currentUser ? "/book" : "/login"} className={styles.navLink}>รายการห้องพัก</Link>
+            <Link href={currentUser ? "/book" : "/login"} className={styles.navLink}>Rooms</Link>
             <Link href="/about" className={styles.navLink}>About</Link>
-            <Link href="/qr_check" className={styles.navLink}>OCR</Link>
+
 
             {currentUser ? (
               <div style={{ position: 'relative' }}>
